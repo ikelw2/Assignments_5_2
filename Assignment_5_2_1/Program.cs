a0ec02e5918d65745f92ea5028df34e4de52a061
Michael Wood <ikelw2@gmail.com>
2026-08-04 01:47:45 +0200

    Add project files.

---
+++ Assignment_5_2_1/Program.cs
@@ -0,0 +1,113 @@
+﻿// 1.Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only.
+// 
+// Example 1:
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+//
+// Example 2:
+// Input: s = " fly me to the moon "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// Input: s = "on the moon tiger choose"
+
+
+Console.WriteLine("Assignment_5_2_1: return length of the last word in the string\n");
+// ----------------------------------
+while (true)
+{
+    // create random arrangement of string with words of different length
+    Random random = new Random();
+    // testing of this worked // string words = "a on\\ the] moon! tiger, choose- briefly. severely3";
+    string words = "a on the moon tiger choose briefly severely";
+    // split by space, order by a random number, and rejoin (used AI to provide test cases)
+    string testStr = string.Join(" ", words.Split(' ').OrderBy(w => random.Next()));
+    // testing of this worked // testStr = "a";
+    // add spaces to add test case, worked // testStr = testStr + " ";
+
+
+
+    // print out test string
+    Console.Write($"Testing '{testStr}' -> ");
+    
+    // test the array, print the results, see below for function
+    Console.WriteLine(GetLengthOfLastWork(testStr));
+
+
+
+
+
+
+
+
+
+
+
+
+    if (DoesUserWantToQuit() == true)
+    {
+        break;
+    }
+}
+// ----------------------------------
+int GetLengthOfLastWork (string s)
+{
+    // start at end of string and work backwards to find the last word
+    
+    // first trim off any trailing spaces
+    s = s.Trim();
+
+    // if string is empty return 0
+    if (s.Length == 0)
+        return 0;
+
+    // method 1: use an integer to count size until space
+    int count = 0;
+    for (int i = (s.Length - 1); i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
+            count++;
+        }
+        else 
+        {
+            if (count > 0) // if count registers length of a word, and we've found a space, break out of for loop
+            {
+                break;
+            }
+        }        
+    }
+
+    return count;
+
+
+
+    //AI proposed something like this, which seems more efficient than the above
+    //    // found the last word, now find the length
+    //    int length = 0;
+    //    while (i > 0 && s[i - 1] != ' ')
+    //    {
+    //        length++;
+    //        i--;
+    //    }
+    //    Console.WriteLine($"Length of last word: {length}");
+    //    return length;
+    //}
+
+
+
+
+}
+// ----------------------------------
+bool DoesUserWantToQuit()
+{
+    Console.WriteLine($"\nPress Q or ESC to quit or press any other key to continue.");
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    if ((keyInfo.Key == ConsoleKey.Q) || (keyInfo.Key == ConsoleKey.Escape))
+    {
+        return true;
+    }
+    Console.WriteLine("-------------------------------\n");
+    return false;
+}
---
+++ Assignment_5_2_2/Program.cs
@@ -0,0 +1,92 @@
+﻿// 2.Write a program in C# Sharp to print the first n natural number using recursion.
+// 
+// Test Data :
+// How many numbers to print : 10
+// Expected Output :
+// 1 2 3 4 5 6 7 8 9 10
+
+Console.WriteLine("Assignment_5_2_2-3: Print first N natural numbers using recursion, both upwards, and downwards \n");
+// ----------------------------------
+while (true)
+{
+    // create random arrangement of string with words of different length
+    Random random = new Random();
+    int randSize = random.Next(15);
+    
+    // print out test string
+    Console.Write($"First {randSize} natural numbers are: \n");
+    
+    
+
+
+    // print out from 1 to x INCREASING
+    recursiveNaturalUp(1, randSize);
+
+    Console.WriteLine();
+
+    // print out from x to 1 DECREASING
+    recursiveNaturalDown(1, randSize);
+
+
+
+
+
+
+    if (DoesUserWantToQuit() == true) { break; }
+}
+// ----------------------------------
+// to print from 1 to x (natural number), I need to:
+//
+// #1 use a BASE CASE to stop the loop
+// #2 print the current number (either #2 or #3 depending upon desired order
+// #3 call recursive function with next number
+//
+// ----------------------------------
+void recursiveNaturalUp(int x, int max)
+{
+    if (x > max) // base case
+        return;
+
+    Console.Write(x + " "); // print the output before the recursive function calls,
+                            // mean it's a HEAD recursion, prints in forward order
+
+    recursiveNaturalUp(x + 1, max); // recursive step - call the function with the next number
+}
+// ----------------------------------
+
+void recursiveNaturalDown(int x, int max)
+{
+    if (x > max) // base case
+        return;
+
+    recursiveNaturalDown(x + 1, max); // recursive step - call the function with the next number
+
+    Console.Write(x + " "); // print the output after the recursive function calls,
+                            // means it's a TAIL recursion, prints in reverse order
+}
+// ----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------
+bool DoesUserWantToQuit()
+{
+    Console.WriteLine($"\nPress Q or ESC to quit or press any other key to continue.");
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    if ((keyInfo.Key == ConsoleKey.Q) || (keyInfo.Key == ConsoleKey.Escape))
+    {
+        return true;
+    }
+    Console.WriteLine("-------------------------------\n");
+    return false;
+}
